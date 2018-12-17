eaaa6506f46440440620f5a3976163022268f7d2
insua <insua@sohu.com>
2018-12-17 20:21:34 +0800

    Not one conflict, today was a good day.

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'flutter',
       home: Scaffold(
-       appBar: new AppBar(
-         title: new Text('ListView Widget'),
+       body: Center(
+         child: Container(
+           height: 200.0,
+           child: MyList()
+         ),
        ),
-        body: new ListView(
-          children: <Widget>[
-           new Image.network(
-             'https://img.ssyer.com/picture/full/Bkd38RxMf7.jpg'
-           ),
-            new Image.network(
-              'https://img.ssyer.com/picture/new/zl/b81360fe84358c0fc5201f76e0691b1e.jpg'
-            ),
-            new Image.network(
-              'https://img.ssyer.com/picture/new/zl/8a90a8fe6c503ab9d8b36cb90355244b.jpg'
-            )
-          ],
-        ),
       ),
     );
+  }
+
+}
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Container(
+          width: 180.0,
+          color: Colors.lightBlue,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.deepOrange,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.deepPurpleAccent,
+        )
+      ],
+    )
   }
 
 }
