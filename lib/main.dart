e90d8fc97915dc3d1675322e5806b93453f0de9a
insua <insua@sohu.com>
2019-03-07 22:20:51 +0800

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

--- lib/main.dart
+++ lib/main.dart
@@ -10,28 +10,15 @@ class MyApp extends StatelessWidget{
         appBar: new AppBar(
           title: new Text('水平方向布局'),
         ),
-        body: new Row(
+        body: Column(
           children: <Widget>[
-            new RaisedButton(onPressed: (){
-              
-            },
-              color: Colors.redAccent,
-              child: new Text('红色按钮'),
-            ),
-            Expanded(child:new RaisedButton(onPressed: () {
-              
-            },
-              color: Colors.orangeAccent,
-              child: new Text('黄色按钮'),
-            )),
-            new RaisedButton(onPressed: (){
-              
-            },
-              color: Colors.pinkAccent,
-              child: new Text('粉色按钮'),
-            )
+            Center(child:Text('苟利国家生死以')),
+            Expanded(child:Center(child:Text('岂因祸福避趋之'))),
+            Center(child:Text('续一秒'))
           ],
-        ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+        )
       ),
     );
   }
