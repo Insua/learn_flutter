82d1a221e0a093b53ed4ebc78dd6e065ddd9eb46
insua <insua@sohu.com>
2019-03-07 23:07:03 +0800

    Some bugs fixed

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,31 @@ void main () => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    var stack = new Stack(
+      alignment: const FractionalOffset(0.5, 0.5),
+      children: <Widget>[
+        new CircleAvatar(
+          backgroundImage: new NetworkImage('https://iocaffcdn.phphub.org//uploads/communities/WtC3cPLHzMbKRSZnagU9.png'),
+          radius: 100.0,
+        ),
+        new Container(
+          decoration: new BoxDecoration(
+            color: Colors.lightBlue
+          ),
+          padding: EdgeInsets.all(5.0),
+          child: new Text('苟日新'),
+        )
+      ],
+    );
+
     return MaterialApp(
       title: 'ListView widget',
 
       home: Scaffold(
         appBar: new AppBar(
-          title: new Text('水平方向布局'),
+          title: new Text('垂直方向布局'),
         ),
-        body: Column(
-          children: <Widget>[
-            Center(child:Text('苟利国家生死以')),
-            Expanded(child:Center(child:Text('岂因祸福避趋之'))),
-            Center(child:Text('续一秒'))
-          ],
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-        )
+        body: Center(child: stack),
       ),
     );
   }
